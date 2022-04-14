a5632fd5a40812004a89030c8042a9efeaee119c
Kishan Prajapati <kishanprajapati@FVFGV627Q05N.local>
2022-04-14 10:33:14 +0530

    code optimized

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
-var bodyParser = require('body-parser');
+
+const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
@@ -11,6 +12,7 @@ app.get('/', (req, res) => {
 app.listen(3333, () => console.log("Server is live at port 3333"));
 
 app.post('/chat', function (req, res) {
+
     var request = require('request');
 
     var city = req.body.queryResult.parameters.city; //fetching city
@@ -22,24 +24,24 @@ app.post('/chat', function (req, res) {
     if (city == null) {
         city = "mumbai";
     }
-
+    console.log("Test");
     request("http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&apikey=a7a7466a61f208c4bb35bed785bd441d", function (error, response, body) {
         console.log('error:', error); // Print the error if one occurred
         console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
 
         var obj = JSON.parse(body);
-        var list = obj.list[0];
-        console.log(list);
+        var list = obj.list;
 
-        for (var i = 0; i < obj.list.length; i++) {
-            var date = new Date(obj.list[i].dt * 1000);
-            var weather = obj.list[i].weather[0].description;
-            if (date.getMonth() == month && date.getDate() == day) {
-                description = "Weather will be like " + weather + " in " + city + " on " + date.toDateString();
-                // description = weather; description = description + " " + date.toDateString();
-                break;
-            }
+        var result = list.filter(getDescription);
+        function getDescription(x) {
+            var date1=new Date(x.dt*1000);
+            return date.getMonth()== month && date.getDate() == day ;
         }
+
+        // console.log("result",result);
+        var description = "Weather will be " + result[1].weather[0].description + " in " + city + " on " + date.toDateString();
+        // console.log("description:",description);
         res.send(JSON.stringify({ "fulfillmentText": description }));
     });
+
 });
